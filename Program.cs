15dcb06c16da95a0a72345ef8edb74cd59baf541
Mr Hall <151441867+MrLHall@users.noreply.github.com>
2024-05-08 09:36:36 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace CHAR_TO_CODE
 {
     internal class Program
     {
-        static int CHAR_TO_CODE(string a) //takes a as a perameter, this willl be the users input
+        static int CHAR_TO_CODE(string a) //takes a as a perameter, this will be the users input
         {
             char letter = Convert.ToChar(a); // converts the inputted letter to a char
             return Convert.ToInt16(letter); //returns the ASCII value 
