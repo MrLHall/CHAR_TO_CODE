98ab7f0a590e4fd2e7249892c671d2e77801d45e
Lang Hall <lhall@heckgrammar.co.uk>
2024-05-08 09:31:04 +0100

    Framework

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ namespace CHAR_TO_CODE
 {
     internal class Program
     {
+        static int CHAR_TO_CODE(string a) //takes a as a perameter, this willl be the users input
+        {
+            char letter = Convert.ToChar(a); // converts the inputted letter to a char
+            return Convert.ToInt16(letter); //returns the ASCII value 
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
